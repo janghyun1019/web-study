47b590c1a628619302b4bf9b0904dbc1b25cdb7f
janghyun1019 <mon.p.a.hyun@gmail.com>
2025-01-17 16:04:41 +0900

    lombok 설정 및 사용

--- src/main/java/com/app/dto/room/Room.java
+++ src/main/java/com/app/dto/room/Room.java
@@ -1,5 +1,38 @@
 package com.app.dto.room;
 
-public class Room {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+
+
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Data
+public class Room { 
+	 
+	int roomId; //Primary Key 기본키 식별자
+	String buildingNumber; //동 이름  A동 B동 
+	int roomNumber;   //호실번호 101 405
+	int floor;   //층   7  8  
+	int maxGuestCount; //최대 숙박 인원
+	String viewType;    // OCN / CTY / MOT
+						// 오션   시티    마운틴
+				//int		1     2     3 
+	//int accomodationId;
 }
+
+
+//전화번호
+// 01012341234
+// 번호1 010
+// 번호2 1234
+// 번호3 1234

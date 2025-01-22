45a0b15dd06e548528618b64a8a4e90b5ee006da
janghyun1019 <mon.p.a.hyun@gmail.com>
2025-01-22 10:28:31 +0900

    회원 가입 추가 - 모닝퀴즈

--- src/main/java/com/app/controller/customer/CustomerController.java
+++ src/main/java/com/app/controller/customer/CustomerController.java
@@ -2,7 +2,10 @@ package com.app.controller.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.app.dto.user.User;
 import com.app.service.user.UserService;
 
 @Controller
@@ -10,4 +13,27 @@ public class CustomerController {
 	
 	@Autowired
 	UserService userService;
+	
+	// 회원가입
+	@GetMapping("/customer/signup")
+	public String singup() {
+		return "customer/signup";
+	}
+	
+	@PostMapping("/customer/signup")
+	public String singupAction(User user) {
+		
+		user.setUserType("CUS");
+		int result = userService.saveUser(user);
+		
+		System.out.println("회원가입 처리 결과 : " + result);
+		
+		if(result > 0) {
+			return "redirect:/main";
+		} else {
+			return "customer/signup";			
+		}
+
+		
+	}
 }
